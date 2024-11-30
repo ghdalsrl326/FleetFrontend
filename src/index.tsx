203bbcc69c4820d95d9575999109f863a000f1d6
minkihong <ghdalsrl326@gmail.com>
2024-11-30 20:23:32 +0900

    setup initial msw

--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "App";
 import reportWebVitals from "reportWebVitals";
-import { SetupWorkerApi } from "msw/browser";
+import { worker } from "mocks/browser";
 
-if (process.env.NODE_ENV === "development") {
-  const { worker }: { worker: SetupWorkerApi } = require("mocks/browser");
-  worker.start({ onUnhandledRequest: "bypass" });
+async function startApp() {
+  if (process.env.NODE_ENV === "development") {
+    // Wait for the worker to be ready
+    await worker.start({ onUnhandledRequest: "bypass" });
+  }
+
+  const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement,
+  );
+
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
 }
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+startApp();
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+// to log results (for example: reportWebVitals.console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
